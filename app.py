7d16f6a890f094443bfee1db6e3af1ba67b2a569
Rochelle <rochelletham@users.noreply.github.com>
2022-02-26 16:08:17 -0600

    Initial files

--- app.py
+++ app.py
@@ -9,13 +9,6 @@ from secret import *
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
-# generic call 
-# sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=lotify_client,
-#                                                            client_secret=lotify_client_secret))
-
-# results = sp.search(q='weezer', limit=20)
-# for idx, track in enumerate(results['tracks']['items']):
-#     print(idx, track['name'])
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=lotify_client,
                                                client_secret=lotify_client_secret,

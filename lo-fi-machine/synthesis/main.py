a7ac947af044f4fc3d1f4e3db2e7d82def5d8cab
quinnouyang <quinn.ouy@gmail.com>
2022-02-26 19:15:41 -0600

    Partially setup processing and synthesis directories

---
+++ lo-fi-machine/synthesis/main.py
@@ -0,0 +1,20 @@
+import json
+import boto3
+
+def main(event, context):
+    s3 = boto3.client('s3')
+    
+    # Output the bucket names
+    print("This is from the synthesis directory AGAIN AGAIN.")
+    response = s3.get_object(
+        Bucket="lo-tify",
+        Key='text.txt',
+    )
+    
+    body = response['Body'].read()
+    print(body)
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
